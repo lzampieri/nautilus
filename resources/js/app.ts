3c0506dcb25d3a253deeca632a059168e007372d
lzampieri <zampieri.leonardo98@gmail.com>
2026-04-06 00:19:57 +0200

    First commit!

---
+++ resources/js/app.ts
@@ -0,0 +1,11 @@
+import { createInertiaApp } from '@inertiajs/vue3';
+import AppLayout from '@/layouts/AppLayout.vue';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+createInertiaApp({
+    title: (title: string) => (title ? `${title} - ${appName}` : appName),
+    layout: () => AppLayout
+});
+
+// This will set light / dark mode on page load...
